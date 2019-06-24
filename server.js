7306c1324e1f805eff2150d3da1583c69cfa0af7
lyhuynhnhu <lyhuynhnhu200699@gmail.com>
2019-06-24 15:55:34 +0700

    change error

--- server.js
+++ server.js
@@ -20,26 +20,20 @@ http.createServer( function(req, res){
     //print file's name
     console.log("request for "+ pathname +" received");
 
-    
-    //write the content file
-
-    // var x=path.basename(req.url)*1;
-    // result=x*2;
-    // res.write(result.toString());
-
-    var id=path.basename(req.url);
+    //Return the last portion of a path
+    var id=path.basename(req.url);  
     var requestedUser = users[id];
 
     if (requestedUser) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("hi "+requestedUser.name+"");
+        res.write("Hi "+requestedUser.name+", your age: "+requestedUser.age);
+        res.end();
 
     } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});       
+        res.writeHead(404, {'Content-Type': 'text/html'});  
+        res.end('<h1>404 not found</h1>');     
     }
-
-    res.end();
-
+    
 }).listen(8080);
 
 console.log('running at http://127.0.0.1:8080/');
